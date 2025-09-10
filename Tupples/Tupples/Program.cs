52ba59043388025ed51e2e4caed83b91d29e106d
Kester Madanga <madangakester@gmail.com>
2025-09-10 21:24:42 +0300

    update

--- Tupples/Tupples/Program.cs
+++ Tupples/Tupples/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace ConsoleApp
+{
+    class CarYardApp
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please select your desired brand (1. Toyota, 2. Nissan): ");
+            int carBrand = Convert.ToInt32(Console.ReadLine());
+
+            string brandChoice = carBrand switch
+            {
+                1 => "Toyota",
+                2 => "Nissan",
+                _ => "Invalid Choice"
+            };
+
+            Console.WriteLine();
+            Console.WriteLine($"You have selected: {brandChoice}");
+
+            if (brandChoice == "Invalid Choice")
+            {
+                Console.WriteLine("Exiting program...");
+                return;
+            }
+
+            Console.Write("Please select a model (1. Sijui, 2. Najua): ");
+            int model = Convert.ToInt32(Console.ReadLine());
+
+            string modelChoice = model switch
+            {
+                1 => "Sijui",
+                2 => "Najua",
+                _ => "Invalid Choice"
+            };
+
+            Console.WriteLine();
+            Console.WriteLine($"You have selected: {brandChoice} - {modelChoice}");
+        }
+    }
+}
